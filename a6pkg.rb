f1f5159b43fb63b5b06bcba1d49ee7a50fdb3525
Jeff Sharp <jeff.sharp@git>
2016-09-11 08:58:18 -0500

    WIP from months ago

--- a6pkg.rb
+++ a6pkg.rb
@@ -18,9 +18,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'optparse'
+require 'pathname'
+
+# os:         os.syx -> os.bin
+# boot:     boot.syx -> boot.bin
+# nvram: patches.syx -> patches/pgm-01-002.bin
 
 module A6
   class Packager
+    attr_reader :options
+
     def run(args)
       @options = parse_options(args)
       send(@options[:mode] || :decode)
@@ -35,98 +42,107 @@ module A6
             options[:mode] = :decode
           end
 
-          p.on("--decode-sysex", "Convert SysEx to blocks.") do
-            options[:mode] = :decode_sysex
+          p.on("-o", "--output FILE", "Write output to file.") do |file|
+            options[:output] = file
+          end
+
+          p.on("-v", "--verbose", "Enable verbose output.") do
+            options[:verbose] = true
           end
 
           p.on("-h", "--help", "Print this help message.") do
             puts p
             exit
           end
-
-          # Future work
-          #o.on("--decode-blocks", "Convert blocks to binary")
-          #o.on("-e", "--encode", "Convert binary to SysEx")
-          #o.on("--encode-binary", "Convert binary to blocks")
-          #o.on("--encode-blocks", "Convert blocks to SysEx")
         end.parse!(args)
       end
     end
 
     def decode
-      sysex_decoder = SysexDecoder.new
-      block_decoder = BlockDecoder.new
+      decoder = options[:decoder]
 
-      each_file do |sysex|
-        sysex_decoder.decode(sysex) do |block|
-          block_decoder.decode(block)
+      each_file do |path, bytes|
+        bytes.scan(SYSEX) do |opcode, data|
+          opcode = opcode.ord
+          decoder ||= decoder_for(opcode)
+          decoder.source = path
+          decoder.pos = $~.begin(0)
+          decoder.decode(opcode, data)
         end
       end
 
-      block_decoder.verify.write_to($stdout.binmode)
+      decoder.flush if decoder
     end
 
-    def decode_sysex
-      sysex_decoder = SysexDecoder.new
-      blocks = ''.b
-
-      each_file do |sysex|
-        sysex_decoder.decode(sysex) do |block|
-          blocks << block
-        end
-      end
-
-      $stdout.binmode << blocks
-    end
-
-    protected
+    private
 
     def each_file
       ARGF.binmode
       loop do
         begin
-          yield ARGF.file.read
-        rescue FormatError => e
-          warn "#{ARGF.filename}: #{e}"
+          yield ARGF.filename, ARGF.file.read
+        rescue FormatError
           exit 1
         end
         ARGF.skip
         break if ARGV.empty?
       end
     end
+
+    def decoder_for(opcode)
+      klass =
+        case opcode
+        when 0x30 then OsDecoder
+        when 0x3F then BootDecoder
+        else           NvramDecoder
+        end
+      klass.new(options)
+    end
+
+    # A6 SysEx Frame Format
+    SYSEX = /
+      \xF0            # SysEx start
+      \x00\x00\x0E    # Manufacturer ID
+      \x1D            # Family ID
+      ([\x00-\x7F])   # Opcode
+      ([\x00-\x7F]*+) # Data
+      \xF7            # SysEx end
+    /xn
   end
 
   class FormatError < StandardError
   end
 
-  class SysexDecoder
-    attr_reader :kind
+  class Decoder
+    attr_accessor :source, :pos
+    attr_reader   :target, :options
 
-    def decode(midi)
-      first_op = nil
-
-      midi.scan(SYSEX) do |op, data|
-        offset = $~.begin(0)
-
-        if !first_op
-          case first_op = op
-          when Op::UPDATE_OS   then @kind = :os
-          when Op::UPDATE_BOOT then @kind = :boot
-          end
-        elsif op != first_op
-          raise FormatError, 'sysex offset %d: expected %02X, but found %02X' % [
-              offset + 5, first_op, op
-          ]
-        end
-
-        yield data_decode(data)
-      end
-      self
+    def initialize(options)
+      @options = options
     end
 
-    private
+    def source=(name)
+      if name && name != '-'
+        @source   = name.to_s
+        @target ||= target_for(@source)
+      else
+        @source   = "(stdin)"
+        @target ||= target_for(nil)
+      end
+      info "scanning #{name}"
+    end
 
-    def data_decode(midi)
+    def decode(opcode, data)
+      # Nothing
+    end
+
+    def flush
+      # Nothing
+    end
+
+    protected
+
+    def decode_7bit(midi)
       "".b.tap do |data|
         offset = 0
         bits8  = 0
@@ -144,83 +160,122 @@ module A6
       end
     end
 
-    # A6 SysEx Frame Format
-    SYSEX = /
-      \xF0            # SysEx start
-      \x00\x00\x0E    # Manufacturer ID
-      \x1D            # Family ID
-      ([\x30\x3F])    # Opcode
-      ([\x00-\x7F]*+) # Data
-      \xF7            # SysEx end
-    /xn
+    def target_for(src)
+      tgt = options[:output] and Pathname.new(tgt)
+    end
 
-    module Op
-      UPDATE_OS   = "\x30"
-      UPDATE_BOOT = "\x3F"
+    def should_write?(file)
+      !file.exist? || !options[:overwrite] or
+        warning "file exists; skipping: #{file}"
+    end
+
+    def require_value(name, value, expected, format = '%s')
+      unless expected === value
+        value    = format % value
+        expected = format % expected
+        error "expected #{name} #{expected}, but found #{value}"
+      end
+    end
+
+    def require_range(name, value, range, format = '%s')
+      unless range.include?(value)
+        value = format % value
+        min   = format % range.min
+        max   = format % range.max
+        error "expected #{name} in range #{min}..#{max}, but found #{value}"
+      end
+    end
+
+    def info(str)
+      $stderr.puts message(str) if options[:verbose]
+    end
+
+    def warning(str)
+      $stderr.puts message(str, :warning)
+    end
+
+    def error(str)
+      $stderr.puts message(str, :error)
+      raise FormatError
+    end
+
+    def message(str, level = nil)
+      "".tap do |s|
+        s << source
+        s << '['  << pos.to_s << ']' if pos
+        s << ': ' << level.to_s      if level
+        s << ': ' << str
+      end
     end
   end
 
   class BlockDecoder
     attr_reader :version, :checksum, :length, :blocks
 
-    def initialize
+    TARGET_EXT = ".bin"
+
+    def initialize(options)
+      super options
       @blocks = []
-      @offset = 0
       @sum    = 0
     end
 
-    def decode(block)
-      unless block.length == BLOCK_LEN
-        invalid 0, "expected block of #{BLOCK_LEN} bytes, but got #{block.length} bytes"
-      end
+    def decode(opcode, data)
+      # Unpack SysEx frame
+      require_value "sysex opcode", opcode,      self.opcode, '%02Xh'
+      require_value "block of",     data.length, BLOCK_LEN,   '%d bytes'
+      data = decode_7bit(data)
 
       # Read header
       version, checksum, length, block_count, block_index =
-        block.unpack('L>3S>2')
+        data.unpack('L>3S>2')
 
-      if @offset == 0
-        @blocks = Array.new(block_count)
-        @version, @checksum, @length, @block_count =
-         version,  checksum,  length,  block_count
+      if !@version
+        # Capture header values from first block
+        @version, @checksum, @length, @block_count, @blocks =
+         version,  checksum,  length,  block_count, Array.new(block_count)
       else
-        require_same  0, 'version',     version,     @version
-        require_same  4, 'checksum',    checksum,    @checksum
-        require_same  8, 'length',      length,      @length
-        require_same 12, 'block count', block_count, @block_count
+        # Validate header
+        require_value 'version',     version,     @version,  '%04Xh'
+        require_value 'checksum',    checksum,    @checksum, '%04Xh'
+        require_value 'length',      length,      @length
+        require_value 'block count', block_count, @block_count
       end
 
-      unless (0...block_count) === block_index
-        invalid 14, "block index #{block_index} is out of range"
-      end
+      # Validate block index
+      require_range 'block index', block_index, 0...block_count
+      warning "duplicate block #{block_index}" if @blocks[block_index]
 
-      if @blocks[block_index]
-        invalid 14, "duplicate block #{block_index}"
-      end
-
+      # Extract data
       length = [[@length - @offset, DATA_LEN].min, 0].max
-      block  = block[HEADER_LEN, length]
+      data   = data[HEADER_LEN, length]
+      sum    = data.each_byte.reduce(:+)
 
-      @blocks[block_index] = block
-      @offset += length
-      @sum     = block.each_byte.reduce(@sum, :+) & 0xFFFFFFFF
+      # Store data
+      @blocks[block_index] = data
+      @sum = (@sum + sum) & 0xFFFFFFFF
       self
     end
 
-    def verify
-      nil_index = @blocks.find_index(nil) and raise FormatError,
-        "one or more blocks missing, starting at block #{nil_index}"
+    def flush
+      pos = nil
 
-      @checksum == @sum or raise FormatError,
-        "header checksum 0x#{'%08X' % @checksum} "\
-        "does not match calculated sum 0x#{'%08X' % @sum}"
+      nil_index = @blocks.find_index(nil) and
+        error "one or more blocks missing, starting at block #{nil_index}"
 
-      self
+      @checksum == @sum or
+        error "calculated sum #{'%08Xh' % @sum} does not match "\
+            "header checksum #{'%08Xh' % @checksum}"
+
+      should_write?(target) and 
+        p "eh" # target.open("wb") { |f| @blocks.reduce(f, :<<) }
     end
 
-    def write_to(io)
-      @blocks.reduce(io, :<<)
-      self
-    end
+    private
+
+    HEADER_LEN =  16 # bytes
+    DATA_LEN   = 256 # bytes
+    BLOCK_LEN  = 311 # bytes, packed 8-to-7 bits
 
     def write_metadata_to(io)
       io.puts \
@@ -231,24 +286,68 @@ module A6
       self
     end
 
-    private
-
-    HEADER_LEN =  16 # bytes
-    DATA_LEN   = 256 # bytes
-    BLOCK_LEN  = HEADER_LEN + DATA_LEN
-
     def format_version(v)
       "#{v / 10000}.#{v / 100 % 100}.#{v % 100}"
     end
+  end
 
-    def require_same(offset, property, actual, expected)
-      unless actual == expected
-        invalid offset, "#{property} does not match previous blocks"
-      end
+  class OsDecoder < BlockDecoder
+    def opcode
+      0x30
     end
 
-    def invalid(offset, reason)
-      raise FormatError, "at blocks offset #{@offset + offset}: #{reason}"
+    def target_for(src)
+      super(src) || Pathname.new(src || "a6-os").sub_ext(".bin")
+    end
+  end
+
+  class BootDecoder < BlockDecoder
+    def opcode
+      0x3F
+    end
+
+    def target_for(src)
+      super(src) || Pathname.new(src || "a6-boot").sub_ext(".bin")
+    end
+  end
+
+  class NvramDecoder < Decoder
+    TARGET_EXT = ".bin"
+
+    def decode(opcode, data)
+      handler = OPCODES[opcode] or
+        warning "unsupported opcode: #{'%02Xh' % opcode}"
+      send handler, data
+    end
+
+    def target_for(src)
+      super(src) || Pathname.new(src || "a6-data").sub_ext("")
+    end
+
+    private
+
+    OPCODES = {
+      0x00 => :op_program
+    }
+
+    PROGRAM_LEN = 2343
+
+    def op_program(data)
+      require_value "program dump of", data.length, PROGRAM_LEN
+
+      bank, program = data.unpack('CC')
+      require_range "bank#",    bank,    0.. 15
+      require_range "program#", program, 0..127
+
+      data = decode_7bit(data[2..-1])
+      file = target + 'pgm-%02d-%03d.bin' % [bank, program]
+
+      info "found bank #{bank}, program #{program}"
+
+      target.mkdir unless target.directory?
+
+      should_write?(file) and
+        file.binwrite data
     end
   end
 end
