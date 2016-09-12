6db6942801d4bd7d2de39d98db3e3f57c8fea73c
Jeff Sharp <jeff.sharp@git>
2016-09-12 06:17:43 -0500

    Add encode_7bit, the inverse of decode_7bit.

--- src/sysex.rs
+++ src/sysex.rs
@@ -14,6 +14,53 @@
 // You should have received a copy of the GNU General Public License
 // along with a6-tools.  If not, see <http://www.gnu.org/licenses/>.
 
+/// Encodes a sequence of bytes into a sequence of 7-bit values.
+///
+pub fn encode_7bit(src: &[u8], dst: &mut Vec<u8>)
+{
+    // Iteration
+    // |  Leftover bits
+    // |  |         7-bit output
+    // |  |         |
+    // 0: ........ 00000000 -> yield 7 bits
+    // 1: .......1 11111110 -> yield 7 bits
+    // 2: ......22 22222211 -> yield 7 bits
+    // 3: .....333 33333222 -> yield 7 bits
+    // 4: ....4444 44443333 -> yield 7 bits
+    // 5: ...55555 55544444 -> yield 7 bits
+    // 6: ..666666 66555555 -> yield 7 bits, then
+    //    ........ .6666666 -> yield 7 bits again
+    // 7: (repeats)
+
+    let mut data = 0u16;    // a shift register where bits become bytes
+    let mut bits = 0;       // how many leftover bits from previous iteration
+
+    for v in src {
+        // Add 8 input bits.
+        data |= (*v as u16) << bits;
+
+        // Yield 7 bits.  Accrue 1 leftover bit for next iteration.
+        dst.push((data & 0x7F) as u8);
+        data >>= 7;
+        bits  += 1;
+
+        // Every 7 iterations, 7 leftover bits have accrued.
+        // Consume them to yield another 7-bit output.
+        if bits == 7 {
+            dst.push((data & 0x7F) as u8);
+            data = 0;
+            bits = 0;
+        }
+    }
+
+    // Yield final leftover bits, if any.
+    if bits > 0 {
+        dst.push((data & 0x7F) as u8);
+    }
+}
+
+/// Decodes a sequence of 7-bit values into a sequence of bytes.
+///
 pub fn decode_7bit(src: &[u8], dst: &mut Vec<u8>)
 {
     // Iteration
@@ -62,15 +109,45 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_encode_7bit() {
+        let data8 = [
+            0xF1, 0xE2, 0xD3, 0xC4, 0xB5, 0xA6, 0x97, 0x88, 0x79, 0x6A,
+        ];
+        let mut data7 = vec![];
+
+        encode_7bit(&data8, &mut data7);
+
+        assert_eq!(data7.len(), 12);
+        //                       always 0
+        //                       | new bits
+        //                       | |    leftover bits
+        //                       | |    |
+        //                    0b_x_xxxx_xxx
+        assert_eq!(data7[ 0], 0b_0_1110001_);
+        assert_eq!(data7[ 1], 0b_0_100010_1);
+        assert_eq!(data7[ 2], 0b_0_10011_11);
+        assert_eq!(data7[ 3], 0b_0_0100_110);
+        assert_eq!(data7[ 4], 0b_0_101_1100);
+        assert_eq!(data7[ 5], 0b_0_10_10110);
+        assert_eq!(data7[ 6], 0b_0_1_101001);
+        assert_eq!(data7[ 7], 0b_0__1001011);
+        assert_eq!(data7[ 8], 0b_0_0001000_);
+        assert_eq!(data7[ 9], 0b_0_111001_1);
+        assert_eq!(data7[10], 0b_0_01010_01);
+        assert_eq!(data7[11], 0b_0_0000_011);
+        //                         |    |
+        //                         |    final leftover bits
+        //                         0-padding
+    }
+
+    #[test]
     fn test_decode_7bit() {
         let data7 = [
-            //
-            //    don't care
-            //    | leftover bits
-            //    | |    new bits
-            //    | |    |
-            // 0b_x_xxxx_xxxx
-            //
+        //     don't care
+        //     | leftover bits
+        //     | |    new bits
+        //     | |    |
+        //  0b_x_xxxx_xxx
             0b_1_1110001_,
             0b_0_100010_1,
             0b_1_10011_11,
