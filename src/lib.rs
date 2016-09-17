a9f0398b147dc7efa60fee339d2bb17789123443
Jeff Sharp <jeff.sharp@git>
2016-09-17 06:07:35 -0500

    Squelch dead_code warnings for now

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with a6-tools.  If not, see <http://www.gnu.org/licenses/>.
 
-    mod io;
+// Squelch noise while experimenting
+#![allow(dead_code)]
+
+pub mod io;
 pub mod sysex;
 
