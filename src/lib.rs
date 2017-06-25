b04edaaceb697de8eab20cc64b33114b0e06b84a
Jeff Sharp <jeff.sharp@git>
2017-06-25 11:20:19 -0700

    Update copyright year.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // This file is part of a6-tools.
-// Copyright (C) 2016 Jeffrey Sharp
+// Copyright (C) 2017 Jeffrey Sharp
 //
 // a6-tools is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published
