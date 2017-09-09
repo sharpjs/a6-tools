4820d7df8286db618c4379a09477e2bcd15d6a31
Jeff Sharp <jeff.sharp@git>
2017-09-08 17:01:12 -0700

    Stub util submodule.

--- src/util.rs
+++ src/util.rs
@@ -14,10 +14,3 @@
 // You should have received a copy of the GNU General Public License
 // along with a6-tools.  If not, see <http://www.gnu.org/licenses/>.
 
-// Squelch noise while experimenting
-#![allow(dead_code)]
-
-pub mod io;
-pub mod sysex;
-pub mod util;
-
